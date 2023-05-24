ec2e83f265615bcf6383a47deb6296e108b0632c
Bjarnthor Egilsson <bjarnthoregilsson@gmail.com>
2023-05-24 14:23:29 +0200

    Update build name

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
-pyinstaller app.py --name ssil --onefile --add-data "static:static" --add-data "templates:templates"
+my_uuid=$(uuidgen)
+dist_name="stupid-simple-image-labels-"$my_uuid
+pyinstaller app.py --name $dist_name --onefile --add-data "static:static" --add-data "templates:templates"
